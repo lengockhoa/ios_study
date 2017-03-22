b59231215a114dc38aca496e3d09c3a14b21a468
Ngoc Khoa Le <ngockhoale@Ngocs-MacBook-Pro.local>
2017-03-22 18:36:50 +0700

    Thêm cái deepcopy cho protocol Hình

--- baitap2.playground/Contents.swift
+++ baitap2.playground/Contents.swift
@@ -11,6 +11,7 @@ protocol Hinh{
     func chuVi() -> Double
     func dienTich() -> Double
     func inThongTin()
+    func clone() -> Hinh
 }
 
 class HinhTron: Hinh{
@@ -36,12 +37,12 @@ class HinhTron: Hinh{
         print("Đây là hình tròn với diện tích là: \(dienTich()) và chu vi là : \(chuVi())")
     }
     
-    func clone() -> HinhTron{
+    func clone() -> Hinh{
         return self
     }
 }
 
-class HinhChuNhat{
+class HinhChuNhat: Hinh{
     var dai: Double
     var rong: Double
     
@@ -62,7 +63,7 @@ class HinhChuNhat{
         print("Đây là hình vuông với diện tích là: \(dienTich()) và chu vi là : \(chuVi())")
     }
     
-    func clone() -> HinhChuNhat{
+    func clone() -> Hinh{
         return self
     }
 }
@@ -72,3 +73,13 @@ let hinhTron2 = HinhTron(tam: Diem(x: 5,y: 8), banKinh: 9)
 let hinhChuNhat1 = HinhChuNhat(dai: 15, rong: 6)
 let hinhChuNhat2 = HinhChuNhat(dai: 22, rong: 11)
 let hinhChuNhat3 = HinhChuNhat(dai: 18, rong: 13)
+
+let hinhs: [Hinh] = [hinhTron1,hinhTron2,hinhChuNhat1,hinhChuNhat2,hinhChuNhat3]
+var hinhscopy: [Hinh] = []
+for hinh in hinhs{
+    hinhscopy.append(hinh.clone())
+}
+
+hinhs[1].chuVi()
+//hinhs[1].banKinh = 13
+hinhscopy[1].chuVi()
